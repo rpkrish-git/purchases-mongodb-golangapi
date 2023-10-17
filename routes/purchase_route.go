7e22f8242cab779e150599b53c1876599c638248
rpkrish <rp.krish@gmail.com>
2023-10-17 09:17:30 -0500

    updated readme

--- routes/purchase_route.go
+++ routes/purchase_route.go
@@ -7,11 +7,10 @@ import (
 )
 
 func PurchaseRoute(router *mux.Router) {
-	//All routes related to users comes here
+	//All routes related to purchases here
 	router.HandleFunc("/purchase", controllers.CreateATransaction()).Methods("POST")
 	router.HandleFunc("/purchases/{transactionId}", controllers.GetATransaction()).Methods("POST")
 	router.HandleFunc("/purchases/{transactionId}", controllers.EditATransaction()).Methods("PUT")
 	router.HandleFunc("/purchases/{transactionId}", controllers.DeleteATransaction()).Methods("DELETE")
 	router.HandleFunc("/purchases", controllers.GetAllPurchases()).Methods("POST")
-	//router.HandleFunc("/health-check", controllers.HealthCheck()).Methods("GET")
 }
